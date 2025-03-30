41890b62840d9f1177f54a4d6d91774451ec1895
Kripasree-coder <kripasreemohanraj@gmail.com>
2025-03-30 12:12:42 +0530

    File modified

---
+++ config.py
@@ -0,0 +1,6 @@
+COINGECKO_URL = "https://api.coingecko.com/api/v3/simple/price"
+HISTORICAL_URL = "https://api.coingecko.com/api/v3/coins/{}/market_chart"
+
+CRYPTO_LIST = ["bitcoin", "ethereum", "dogecoin"]
+
+REFRESH_TIME = 10  # Auto-refresh time in seconds
