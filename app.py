96e8b9ed5d8d2387a106f10a036ce8ea7a925c58
Kripasree-coder <kripasreemohanraj@gmail.com>
2025-03-30 12:51:39 +0530

    File modified

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from ui_components import display_live_prices, display_crypto_trend, display_abo
 
 # Set Page Config
 st.set_page_config(page_title="Crypto Dashboard", page_icon="🚀", layout="wide")
-
+st.image("C:/Users/kripa/OneDrive/Desktop/PROJECTS/AI/AI_NEWS_SCRAPPER/1.webp", caption="Live Crypto Trends")
 # Sidebar Navigation
 st.sidebar.title("🚀 Crypto Dashboard")
 st.sidebar.write("📌 Track live prices & trends of your favorite cryptocurrencies.")
